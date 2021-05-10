8b65cb70c6e4c8367cf4f6cf8bcf0569475ea2df
hhwnas <cbxnz0@naver.com>
2021-05-10 21:18:05 +0900

    05-10

---
+++ src/01_java/bigData/src/com/lex/ex/VacEx01.java
@@ -0,0 +1,15 @@
+package com.lex.ex;
+
+public class VacEx01 {
+	public static void main(String[] args) {
+		int age = 34;
+		// 타입 변수형 = (할당연산자) 초기값;
+		String name = "호환";
+		System.out.println("안녕하세요 "+age+"살 "+name+"씨~ ");
+		age =age + 1 ;
+		System.out.println("안녕하세요 "+age+"살 "+name+"씨~ ");
+	}
+	
+
+	
+}
